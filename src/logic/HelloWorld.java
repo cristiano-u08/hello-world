c6f50a94d38eec795617e24d665c7ba7fc2fe0f2
cristiano-u08 <cristia_no98@live.it>
2021-03-15 14:31:34 +0100

    added comment in HelloWorld.java

--- src/logic/HelloWorld.java
+++ src/logic/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	private static final Logger LOGGER = Logger.getLogger("Hello World!");
 	
 	public static void main(String[] args) {
-		
+		// utilizzo di Logger per evitare smell su sonar
 		LOGGER.info(LOGGER.getName());
 	}
 
